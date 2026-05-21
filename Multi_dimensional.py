677fb054a532a7bbef6f8a29e6b0a17efecc037b
Abhishek Thakur <abhishek180717@gmail.com>
2026-05-21 18:06:15 +0530

    Hotel

--- Multi_dimensional.py
+++ Multi_dimensional.py
@@ -53,5 +53,12 @@
 # print(hot_days, "days were hot days in the month.") 
 
 rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
-
 print(rooms)
+rooms[1][9][13] = True
+rooms[1][9][1] = False
+vacancy = 0
+for room_number in range(20):
+    if not rooms[1][9][room_number]:
+        vacancy += 1
+print("Vacany rooms", vacancy)
+        
