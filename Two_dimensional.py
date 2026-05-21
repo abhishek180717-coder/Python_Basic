274f7b82830d8ace636d1d335a9902e15e41c73b
Abhishek Thakur <abhishek180717@gmail.com>
2026-05-21 17:14:22 +0530

    Chess game

--- Two_dimensional.py
+++ Two_dimensional.py
@@ -25,5 +25,36 @@ board[7][7] = "ROOKS"
 print("**********")
 for index in board:
     print(index)
+    
+board = []
+for i in range(8):
+    row = ["EMPTY" for i in range(8)]
+    board.append(row)
+# print(board)    
+for index in board:
+    print(index)
+# print(len(board))
+# print(board[0][0])
+board[0][0] = "ROOKS"
+board[0][7] = "ROOKS"
+board[7][0] = "ROOKS"
+board[7][7] = "ROOKS"
+
+board[0][2] = "BISHOP"
+board[0][5] = "BISHOP"
+board[7][2] = "BISHOP"
+board[7][5] = "BISHOP"
+
+board[0][3] = "QUEEN"
+board[0][4] = "KING"
+board[7][3] = "QUEEN"
+board[7][4] = "KING"
+
+board[1] = ["PAWN" for i in range(8)]
+board[6] = ["PAWN" for i in range(8)]
+
+print("**********")
+for index in board:
+    print(index)    
 
 
