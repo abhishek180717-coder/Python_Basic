0d97ac4eb75a12db96545a96903ad3730784fec1
ABHISHEK THAKUR <abhishek180717@gmail.com>
2026-05-26 18:03:07 +0530

    Tuple

---
+++ recursion.py
@@ -0,0 +1,38 @@
+# def fun (num):
+#     if num<0:
+#         return
+#     print(num)
+#     fun(num-1)
+# fun(5)    
+
+# def countDown(number):
+#     print(number)
+#     if number == 0:
+#         return
+#     else:
+#         print("Going in rec:", number)
+#         countDown(number -1)
+#         print("out of rec:", number)
+
+# print("Starting Recursion")
+# countDown(5)
+# print("Completed Recursion")        
+        
+# def fact(num):
+#     global x
+#     x = x * num
+#     if num == 1:
+#         return
+#     fact(num -1)
+    
+# x = 1
+# fact(5)
+# print(x)            
+
+
+def factorial(number):
+    if number <= 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
+print(factorial(5))    
