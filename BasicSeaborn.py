44cf07b88568717271bd4ae1ce48b5966f14efc8
ABHISHEK THAKUR <abhishek180717@gmail.com>
2026-06-16 18:07:07 +0530

    Seaborn Program in Python

---
+++ BasicSeaborn.py
@@ -0,0 +1,15 @@
+# import matplotlib.pyplot as plt
+
+# import seaborn as sns 
+# import pandas as pd
+# import numpy as np
+# np.random.seed(42)
+# df = pd.DataFrame({
+#     'marks':       np.random.randint(40,100,100),
+#     'study_hours': np.random.uniform(2,10,100),
+#     'city':        np.random.choice(['Bhopal','Indore','Japalpur'],100),
+#     'gender':      np.random.choice(['Male','Female'],100)})
+# plt.figure(figsize=(10,4))
+# sns.histplot(df['marks'], bins=20, kde=True, color='steelblue')
+# plt.title('Distribution of Students Marks')
+# plt.show()
