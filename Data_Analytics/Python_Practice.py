ee167325f8abe45461a011d3fa4a774691369e69
ABHISHEK THAKUR <abhishek180717@gmail.com>
2026-08-05 13:53:34 +0530

    Day-1 Practice

---
+++ Data_Analytics/Python_Practice.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+print("Hello", name)
+
+Age = int(input("Enter your age:"))
+print("Next year your age is:", Age+1)
--- Practice.py
+++ Practice.py
@@ -440,14 +440,48 @@
       
 # Calculator in Python (100 Days of Code with Harry):-
 
-a = 15
-b = 7
+# a = 15
+# b = 7
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a ** b)
-print(a / b)
-print(a // b)
-print(a % b)      
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a ** b)
+# print(a / b)
+# print(a // b)
+# print(a % b)      
  
+ 
+ 
+
+# Arithmetic Operations Program:-
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# print("Choose operation:")
+# print("1. Addition")
+# print("2. Subtraction")
+# print("3. Multiplication")
+# print("4. Division")
+
+# choice = input("Enter your choice (1/2/3/4): ")
+
+# if choice == "1":
+#     print("Result =", num1 + num2)
+# elif choice == "2":
+#     print("Result =", num1 - num2)
+# elif choice == "3":
+#     print("Result =", num1 * num2)
+# elif choice == "4":
+#     if num2 != 0:
+#         print("Result =", num1 / num2)
+#     else:
+#         print("Error: Division by zero is not allowed.")
+# else:
+#     print("Invalid choice!")
+
+
+
+name = "Harry"
+print(name[-4:-2])
