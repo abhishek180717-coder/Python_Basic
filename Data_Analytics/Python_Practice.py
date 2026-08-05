c7b5349f9771e39e56a6cde5d13a9fae2743a368
ABHISHEK THAKUR <abhishek180717@gmail.com>
2026-08-05 14:21:59 +0530

    Day-2 Practice

--- Data_Analytics/Python_Practice.py
+++ Data_Analytics/Python_Practice.py
@@ -1,5 +1,54 @@
-name = input("Enter your name:")
-print("Hello", name)
+#Day-1:-
 
-Age = int(input("Enter your age:"))
-print("Next year your age is:", Age+1)
+# name = input("Enter your name:")
+# print("Hello", name)
+
+# Age = int(input("Enter your age:"))
+# print("Next year your age is:", Age+1)
+
+
+#Day-2:-
+
+#Even/Odd Numbers?
+# num = int(input("Enter a number:"))
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+    
+# #Largest of 3 Numbers:-
+# a = int(input("Enter a First number:"))   
+# b = int(input("Enter a Second number:"))
+# c = int(input("Enter a Third number:"))
+
+# if a>=b and a>=c:
+#     print("Largest number is:", a)
+# elif b>=a and b>=c:
+#     print("Largest number is:", b)
+# else:
+#     print("Largest number is:", c)  
+    
+    
+    
+# #Multiplication Table:-
+# num = int(input("Enter a number:"))
+
+# for i in range (1, 11):
+#     print(num, "x", i, "=", num * i)  
+    
+    
+
+#Sum of Numbers from 1 to 100:-
+Total = 0
+
+for i in range(1, 101):
+    Total += i
+
+print("Sum = ", Total)   
+
+
+
+        
+               
